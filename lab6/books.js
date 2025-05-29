24365af8232f4db1076e2fe48ddacfec24ebb735
Вика Сопова <vksspv@gmail.com>
2025-05-29 19:49:54 +0700

    Список книг

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: "Фёдор Достоевский",
+        name: "Преступление и наказание",
+        publicationYear: 1866,
+        price: 450,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: "Лев Толстой",
+        name: "Война и мир",
+        publicationYear: 1869,
+        price: 890,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: "Антуан де Сент-Экзюпери",
+        name: "Маленький принц",
+        publicationYear: 1943,
+        price: 320,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: "Джордж Оруэлл",
+        name: "1984",
+        publicationYear: 1949,
+        price: 380,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: "Михаил Булгаков",
+        name: "Мастер и Маргарита",
+        publicationYear: 1967,
+        price: 510,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: "Джоан Роулинг",
+        name: "Гарри Поттер и философский камень",
+        publicationYear: 1997,
+        price: 670,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: "Джон Р. Р. Толкин",
+        name: "Властелин колец: Братство Кольца",
+        publicationYear: 1954,
+        price: 720,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: "Эрих Мария Ремарк",
+        name: "Три товарища",
+        publicationYear: 1936,
+        price: 430,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: "Рэй Брэдбери",
+        name: "451 градус по Фаренгейту",
+        publicationYear: 1953,
+        price: 350,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: "Пауло Коэльо",
+        name: "Алхимик",
+        publicationYear: 1988,
+        price: 290,
+        bestseller: true,
+        available: false
+    }
+];
